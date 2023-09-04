838dd1be67027b18e20c0586e92164c96ff629d3
Alex Barron <alex@abarron.com>
2023-09-04 15:40:18 +0200

    Add create apps controller method and views

--- app/controllers/developer_apps_controller.rb
+++ app/controllers/developer_apps_controller.rb
@@ -7,4 +7,25 @@ class DeveloperAppsController < ApplicationController
     def show
         @developer_app = DeveloperApp.find(params[:id])
     end
+
+    def new
+        @developer_app = DeveloperApp.new
+    end
+
+    def create
+        @developer_app = DeveloperApp.new(developer_app_params)
+
+        if @developer_app.save
+            redirect_to @developer_app
+            flash[:success] = "Developer app successfully created"
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+        
+        def developer_app_params
+            params.require(:developer_app).permit(:name)
+        end
 end
