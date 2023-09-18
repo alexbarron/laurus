ae0cc8e70b3f0c3269482047b940c25462f5f42e
Alex Barron <alex@abarron.com>
2023-09-18 15:17:32 +0200

    Fix helper to handle app creation event

--- app/helpers/developer_apps_helper.rb
+++ app/helpers/developer_apps_helper.rb
@@ -2,10 +2,13 @@ module DeveloperAppsHelper
     def render_changelog_event(version)
         user = User.find(version.whodunnit)
         result = ""
-        
-        version.changeset.each do |key,value|
-            change_item = "<li>#{version.created_at.strftime("%d %b %Y at %H:%M")}: #{user.name} changed #{key} from #{value[0]} to #{value[1]}</li>"
-            result += change_item
+        if version.event == "create"
+            result = "<li>#{version.created_at.strftime("%d %b %Y at %H:%M")}: #{user.name} created app: #{version.changeset["name"][1]}</li>"
+        elsif version.event == "update"
+            version.changeset.each do |key,value|
+                change_item = "<li>#{version.created_at.strftime("%d %b %Y at %H:%M")}: #{user.name} changed #{key} from #{value[0]} to #{value[1]}</li>"
+                result += change_item
+            end
         end
         return result.html_safe
     end
