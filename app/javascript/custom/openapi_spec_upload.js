da6379dc04f143fe9981ade725ba3731aedacc80
Alex Barron <alex@abarron.com>
2023-11-08 16:27:55 +0100

    Fix file extension checker for OpenAPI spec uploads

--- app/javascript/custom/openapi_spec_upload.js
+++ app/javascript/custom/openapi_spec_upload.js
@@ -2,26 +2,14 @@
 
 document.addEventListener("turbo:load", function() {
     document.addEventListener("change", function(event) {
-        var openapi_file = document.getElementById('openapi_spec').value.toUpperCase();
-        console.log(openapi_file);
-        var extension=".YML";
-        var extension2=".YAML";
-        if (openapi_file.indexOf(extension, openapi_file.length - extension.length) == -1||
-        openapi_file.indexOf(extension2, openapi_file.length - extension2.length) == -1) {
+        if (!hasExtension('openapi_spec',['.yml','.yaml'])) {
             alert("File type not allowed,\nAllowed file types: *.yml,*.yaml");
             document.getElementById('openapi_spec').value='';
         }
     });
 });
 
-
-function check_file(){
-    str=document.getElementById('fileToUpload').value.toUpperCase();
-var extension=".YML";
-var extension2=".YAML";
-if(openapi_file.indexOf(extension, openapi_file.length - extension.length) == -1||
-openapi_file.indexOf(extension2, openapi_file.length - extension2.length) == -1){
-alert('File type not allowed,\nAllowed file: *.jpg,*.jpeg');
-document.getElementById('fileToUpload').value='';
-}
+function hasExtension(inputID, exts) {
+    var fileName = document.getElementById(inputID).value;
+    return (new RegExp('(' + exts.join('|').replace(/\./g, '\\.') + ')$', "i")).test(fileName);
 }
